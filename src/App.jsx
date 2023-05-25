55ce9ecda51e550b21072fb422d3dd813f43d029
Martinserrano724 <martinserrano724@gmail.com>
2023-05-25 13:01:12 -0300

    Creacion del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Empleado from './components/Empleado';
+
+function App() {
+
+  return (
+    <>
+      <Empleado></Empleado>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Empleado.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Empleado = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Empleado;
